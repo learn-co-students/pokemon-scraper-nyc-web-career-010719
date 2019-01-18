42a2c807f5d0125209407742f5191cb4ff2bb9f4
davidfloyd91 <davidfloyd91@gmail.com>
2019-01-18 14:35:53 -0500

    one test left

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+
+  attr_reader :id, :name, :type, :db
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(id:, name:, type:, db:)
+    @id = @@all.length + 1
+    @name = name
+    @type = type
+    @db = db
+    @@all << self
+  end
+
+  def self.save(name, type, db)
+    pokemon = Pokemon.all.find do |poke|
+      poke.name == name && poke.type == type
+    end
+    id = pokemon.id
+    sql = "INSERT INTO pokemon(id, name, type) VALUES (?, ?, ?)"
+    db.execute(sql, id, name, type)
+  end
+
 end
